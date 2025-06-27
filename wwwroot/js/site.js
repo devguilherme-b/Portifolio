e20fe215ffc462cf6303ad614bbda8ec7ef65d7d
devguilherme-b <hitsnoplaymusic@gmail.com>
2025-06-27 17:29:59 -0300

    Refactor layout responsiveness

---
+++ wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    particlesJS.load('particles-js', '/particles/particles.json', function () {
+        console.log('particles.js config loaded');
+    });
+});
